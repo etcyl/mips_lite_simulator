b62656b7df960212113456600c5e9e00d4984bea
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2019-05-13 21:44:21 -0700

    debug functional mode + menu select before file rd

--- main.py
+++ main.py
@@ -3,24 +3,29 @@
 
 import sys
 
+# Debugger mode list representing the program
+prog_lst = [0xFFFFFFFF, # First program instruction
+            0x00853000] # Second program instruction
 
 # PC for program counter
 PC = 0
 
 # R for register
-R=[0]*31
+R = [0] * 31
 
 # Total number of instructions and a breakdown of instruction frequencies for the following instruction types:
 # Arithmetic, Logical, Memory Access, Control Transfer
 total_instructions = 0
-arithmetic_inst = 0 # Number of arithmetic instructions
-arithmetic_freq = 0 
+arithmetic_inst = 0  # Number of arithmetic instructions
+arithmetic_freq = 0
 logical_inst = 0
 logical_freq = 0
 mem_inst = 0
 mem_freq = 0
 cntrl_inst = 0
 cntrl_freq = 0
+
+
 
 def set_frequencies():
     if arithmetic_inst >= 1 and total_instructions >= 1:
@@ -31,20 +36,21 @@ def set_frequencies():
         else:
             print('error setting arithmetic freq: total_instructions == 0')
             return
-        
+
     if logical_instr >= 1:
         logical_freq = float(logical_instr) / float(total_instructions)
-    
+
     if mem_inst >= 1:
         mem_freq = float(mem_inst) / float(total_instructions)
-        
+
     if cntrl_inst >= 1:
         cntrl_freq = float(cntrl_inst) / float(total_instructions)
-    
-    print('set instruction frequencies')
-    return    
 
-def r_type(op,rs,rt,rd):
+    print('set instruction frequencies')
+    return
+
+
+def r_type(op, rs, rt, rd):
     # R-type according to opcode:
     # ADD: 000000, SUB: 000010, MUL: 000100, OR: 000110, AND: 001000, XOR: 001010
     #   opcode      rs	    rt	    rd	   Un-use
@@ -54,96 +60,84 @@ def r_type(op,rs,rt,rd):
     if op == 0b000000:
 
         R[rd] = R[rs] + R[rt]
-        print('ADD R'+str(rd)+','+' R'+str(rs)+', R'+str(rt))
-        
-    # SUB    
+        print('ADD R' + str(rd) + ',' + ' R' + str(rs) + ', R' + str(rt))
+
+    # SUB
     elif op == 0b000010:
-        
+
         R[rd] = R[rs] - R[rt]
-        print('SUB R'+str(rd)+','-' R'+str(rs)+', R'+str(rt))
-        
+        print('SUB R' + str(rd) + ',' - ' R' + str(rs) + ', R' + str(rt))
+
     # MUL
     elif op == 0b000100:
-        
+
         R[rd] = R[rs] * R[rt]
-        print('MUL R'+str(rd)+','*' R'+str(rs)+', R'+str(rt))
-        
+        print('MUL R' + str(rd) + ',' * ' R' + str(rs) + ', R' + str(rt))
+
     # OR
     elif op == 0b000110:
-        
+
         R[rd] = R[rs] | R[rt]
-        print('OR R'+str(rd)+','|' R'+str(rs)+', R'+str(rt))
-        
+        print('OR R' + str(rd) + ',' | ' R' + str(rs) + ', R' + str(rt))
+
     # AND
     elif op == 0b001000:
-        
+
         R[rd] = R[rs] & R[rt]
-        print('AND R'+str(rd)+','&' R'+str(rs)+', R'+str(rt))
-        
-     # XOR
+        print('AND R' + str(rd) + ',' & ' R' + str(rs) + ', R' + str(rt))
+
+    # XOR
     elif op == 0b001010:
-        
+
         R[rd] = R[rs] ^ R[rt]
-        print('XOR R'+str(rd)+','^' R'+str(rs)+', R'+str(rt))     
-    
+        print('XOR R' + str(rd) + ',' ^ ' R' + str(rs) + ', R' + str(rt))
+
     else:
         print('r_type opcode error: default case used instead')
-  
-        
 
-def i_type(op,rs,rt,imm):
+
+def i_type(op, rs, rt, imm):
     # I-type according to opcode:
     # ADDI: 000001, SUBI: 000011, MULI: 000101, ORI: 000111, ANDI: 001001, XORI: 001011
     # LDW: 001100, STW: 001101
     # I- TYPE
     #   opcode      rs	    rt	    imm
     #    6b         5b	    5b	    16b
-
     # ADDI
     if op == 0b000001:
-
         R[rd] = int(R[rs]) + int(R[rt])
-        print('ADDI R'+str(rd)+','+' R'+str(rs)+', R'+str(rt))
-        
-    # SUBI    
+        print('ADDI R' + str(rd) + ',' + ' R' + str(rs) + ', R' + str(rt))
+    # SUBI
     elif op == 0b000011:
-        
         R[rd] = int(R[rs]) - int(R[rt])
-        print('SUBI R'+str(rd)+','-' R'+str(rs)+', R'+str(rt))
-        
+        print('SUBI R' + str(rd) + ',' - ' R' + str(rs) + ', R' + str(rt))
     # MULI
     elif op == 0b000101:
-        
         R[rd] = int(R[rs]) * int(R[rt])
-        print('MULI R'+str(rd)+','*' R'+str(rs)+', R'+str(rt))
-        
+        print('MULI R' + str(rd) + ',' * ' R' + str(rs) + ', R' + str(rt))
+
     # ORI
     elif op == 0b000111:
-        
         R[rd] = int(R[rs]) | int(R[rt])
-        print('ORI R'+str(rd)+','|' R'+str(rs)+', R'+str(rt))
-        
+        print('ORI R' + str(rd) + ',' | ' R' + str(rs) + ', R' + str(rt))
     # ANDI
     elif op == 0b001001:
-        
         R[rd] = int(R[rs]) & int(R[rt])
-        print('ANDI R'+str(rd)+','&' R'+str(rs)+', R'+str(rt))
-        
-     # XORI
+        print('ANDI R' + str(rd) + ',' & ' R' + str(rs) + ', R' + str(rt))
+    # XORI
     elif op == 0b001011:
-        
         R[rd] = int(R[rs]) ^ int(R[rt])
-        print('XORI R'+str(rd)+','^' R'+str(rs)+', R'+str(rt))     
-    
+        print('XORI R' + str(rd) + ',' ^ ' R' + str(rs) + ', R' + str(rt))
+    # Default exception case
     else:
         print('I_type opcode error: default case used instead')
 
 
 def simulator(ist):
     global PC
-    for PC in range(0,len(ist)):
-        #converted to hex then binary
-        ist[PC]=int(ist[PC],16)
+    for PC in range(0, len(ist)):
+        # converted to hex then binary
+        ist[PC] = int(ist[PC], 16)
         print(bin(ist[PC]))
 
         # R- TYPE
@@ -154,69 +148,108 @@ def simulator(ist):
         #    6b         5b	    5b	    16b
 
         # shift right to the right 5+5+5+11=26 opcode
-        opcode = ist[PC]>>26
-
+        opcode = ist[PC] >> 26
 
         # Decode go here
 
         # R-type according to opcode:
         # ADD: 000000, SUB: 000010, MUL: 000100, OR: 000110, AND: 001000, XOR: 001010
-        if opcode == 0b000000 or opcode == 0b000010 or opcode == 0b000100  or\
-           opcode == 0b00100 or opcode == 0b001010:
-            #getting rs 5b by masking and shifting
+        if opcode == 0b000000 or opcode == 0b000010 or opcode == 0b000100 or \
+                opcode == 0b00100 or opcode == 0b001010:
+            # getting rs 5b by masking and shifting
 
-            print('opcode ='+bin(opcode))
+            print('opcode =' + bin(opcode))
             # 5+5+11=21 to the rght
-            rs= (ist[PC]>>21) & 0b00000011111
-            #print(bin(rs))
+            rs = (ist[PC] >> 21) & 0b00000011111
+            # print(bin(rs))
 
             # 5+11=16 to the right
-            rt=(ist[PC]>>16) & 0b0000000000011111
-            #print(bin(rt))
+            rt = (ist[PC] >> 16) & 0b0000000000011111
+            # print(bin(rt))
 
             # 11
-            rd=(ist[PC]>>11) & 0b000000000000000011111
-            #print(bin(rd))
+            rd = (ist[PC] >> 11) & 0b000000000000000011111
+            # print(bin(rd))
 
-
-            r_type(opcode,rs,rt,rd)
-
-
-
-
+            r_type(opcode, rs, rt, rd)
 
         # I-type according to opcode:
         # ADDI: 000001, SUBI: 000011, MULI: 000101, ORI: 000111, ANDI: 001001, XORI: 001011
         # LDW: 001100, STW: 001101
 
-        #CONTROL FLOW INSTRUCTION
+        # CONTROL FLOW INSTRUCTION
         # BZ: 001110, BEQ: 001111, JR: 010000, HALT: 010001
         # SPECIAL CASE BZ, JR, HALT does not use all the field in I format
 
 
+def debug_functional(ist):
+    global PC
+    for PC in range(0, len(ist)):
+        # converted to hex then binary
+        ist[PC] = int(ist[PC])
+        print(bin(ist[PC]))
 
+        # R- TYPE
+        #   opcode      rs	    rt	    rd	   Un-use
+        #    6b         5b	    5b	    5b	    11b
+        # I- TYPE
+        #   opcode      rs	    rt	    imm
+        #    6b         5b	    5b	    16b
 
+        # shift right to the right 5+5+5+11=26 opcode
+        opcode = ist[PC] >> 26
 
+        # Decode go here
 
+        # R-type according to opcode:
+        # ADD: 000000, SUB: 000010, MUL: 000100, OR: 000110, AND: 001000, XOR: 001010
+        if opcode == 0b000000 or opcode == 0b000010 or opcode == 0b000100 or \
+                opcode == 0b00100 or opcode == 0b001010:
+            # getting rs 5b by masking and shifting
 
+            print('opcode =' + bin(opcode))
+            # 5+5+11=21 to the rght
+            rs = (ist[PC] >> 21) & 0b00000011111
+            # print(bin(rs))
+
+            # 5+11=16 to the right
+            rt = (ist[PC] >> 16) & 0b0000000000011111
+            # print(bin(rt))
+
+            # 11
+            rd = (ist[PC] >> 11) & 0b000000000000000011111
+            # print(bin(rd))
+
+            r_type(opcode, rs, rt, rd)
+
+        # I-type according to opcode:
+        # ADDI: 000001, SUBI: 000011, MULI: 000101, ORI: 000111, ANDI: 001001, XORI: 001011
+        # LDW: 001100, STW: 001101
+
+        # CONTROL FLOW INSTRUCTION
+        # BZ: 001110, BEQ: 001111, JR: 010000, HALT: 010001
+        # SPECIAL CASE BZ, JR, HALT does not use all the field in I format
 
 def main():
-
-    #try:
+    # try:
     # Read Memory trace by lines
-    with open(sys.argv[1], 'r') as my_file:
-        # read line by line
-        ist = my_file.read().splitlines()
 
     print("""MIPS simulation Enter option (1-3):\n
     1) Functional simulator only\n
     2) Functional simulator + Timing simulator assuming no pipeline forwarding\n
-    3) Functional simulator + Timing simulator with pipeline forwarding\n""")
+    3) Functional simulator + Timing simulator with pipeline forwarding\n
+    4) Functional simulator debug mode (list of instructions)\n""") # Pass a list of instructions to test
 
     option = input('Enter Number:')
 
-    if int(option) ==1:
+    if int(option) == 1:
+        with open(sys.argv[1], 'r') as my_file:
+            # read line by line
+            ist = my_file.read().splitlines()
         simulator(ist)
+    elif int(option) == 4:
+        debug_functional(prog_lst)
+
 
     # except :
     #     print('Check filename again!')
