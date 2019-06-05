4c4b7e5baae5c5f67dfc8567d095ace7b251f272
Phong Nguyen <43973882+Phasor2@users.noreply.github.com>
2019-06-04 19:27:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from simulator import Simulator
 from simulator_no_forwarding import Simulator_no_forwarding
+from simulator_forwarding import Simulator_forwarding
 
 def main():
-    #simulator = Simulator()
-    #simulator.show_instruction = False
-    #simulator.simulation()
-
-
+    simulator = Simulator()
     simulator_no_forwarding = Simulator_no_forwarding()
+    simulator_forwarding = Simulator_forwarding()
+    # Read Memory trace by lines
+    print("""MIPS simulation Enter option (1-4):\n
+     1) Functional simulator only\n
+     2) Functional simulator + Timing simulator assuming no pipeline forwarding\n
+     3) Functional simulator + Timing simulator with pipeline forwarding\n""")  # Pass a list of instructions to test
 
-    simulator_no_forwarding.simulation()
+    option = input('Enter Number:')
+
+    if int(option) == 1:
+        simulator.simulation()
+    elif int(option) == 2:
+        simulator_no_forwarding.simulation()
+    elif int(option) == 3:
+        simulator_forwarding.simulation()
+    else:
+        print('invalid input')
 
 
 
